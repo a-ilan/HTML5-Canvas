5641b072e4ab3e5851be8329bd9c6c301fb7d4a4
Amitai Ilan <amitai.ilan@gmail.com>
2019-02-17 14:57:12 -0500

    build

---
+++ build/precache-manifest.5ed2a361a9bace5b9777c03a7d9b72df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "fa5d72b97105d3079ebe",
+    "url": "./static/js/main.fa5d72b9.chunk.js"
+  },
+  {
+    "revision": "6cb5e100adfa912df375",
+    "url": "./static/js/2.6cb5e100.chunk.js"
+  },
+  {
+    "revision": "63079c8566496930f2efbcb56b8b4d36",
+    "url": "./index.html"
+  }
+];
